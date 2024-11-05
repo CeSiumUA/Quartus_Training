8ce3deb24bb760e0c5dbcf1a818cf21b903d769f
CeSium <fedir.katushonok@gmail.com>
2024-11-05 07:21:53 +0200

    Add PLL instantiation and configuration files for StateMachineProject

--- StateMachineProject/src/StateMachine.vhd
+++ StateMachineProject/src/StateMachine.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 entity StateMachineProject is
     port
     (
+        rst     : in std_logic;
         clk     : in std_logic;
         sw      : in std_logic_vector(3 downto 1);
         led     : out std_logic_vector(3 downto 1)
@@ -12,14 +13,35 @@ end entity;
 
 architecture rtl of StateMachineProject is
 
+component PLL IS
+	PORT
+	(
+		areset		: IN STD_LOGIC  := '0';
+		inclk0		: IN STD_LOGIC  := '0';
+		c0		    : OUT STD_LOGIC 
+	);
+END component;
+
 type DataTypeOfSMState is (STATE1, STATE2, STATE3);
 signal StateVariable : DataTypeOfSMState;
+signal clk_25MHz     : std_logic;
 
 begin
 
-    Process1: process(clk)
+    PLL1 : PLL
+	port map
+	(
+		areset		=> not(rst),
+		inclk0		=> clk,
+		c0		    => clk_25MHz
+	);
+
+    Process1: process(rst, clk_25MHz)
     begin
-        if rising_edge(clk) then
+        if rst = '0' then
+            StateVariable <= STATE1;
+            led <= "000";
+        elsif rising_edge(clk_25MHz) then
 
             case StateVariable is
                 when STATE1 =>
